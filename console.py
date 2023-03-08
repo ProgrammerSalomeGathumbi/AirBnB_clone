c15fb78144a457adc4869845685f6ead4bc9dfe7
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-03-08 13:37:59 +0300

    Added console.py

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" command interpreter module """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Command interpreter prompt """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """ Quit command to exit the program\n """
+        return True
+
+    def do_EOF(self, line):
+        """ EOF command to exit the program\n """
+        print() 	
+        return True
+
+    def emptyline(self):
+        """Enter and empty line dont execute """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
